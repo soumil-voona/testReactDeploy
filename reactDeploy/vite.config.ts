64481dae09a5bbe0c0b8676477e03a666173310a
Soumil Voona <voonasoumil@gmail.com>
2025-05-12 18:20:36 -0500

    asdf

--- reactDeploy/vite.config.ts
+++ reactDeploy/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/testReactDeploy/', // Set the base path to match the GitHub Pages repository name
+  build: {
+    outDir: 'build', // Set the output directory to 'build'
+  },
 })
